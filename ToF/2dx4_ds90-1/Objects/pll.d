fbd0d9bda22611ccbd82a0722b0d6b62d82691a4
Ghckodak <58378180+Ghckodak@users.noreply.github.com>
2020-07-14 16:54:51 -0400

    Add files via upload

---
+++ ToF/2dx4_ds90-1/Objects/pll.d
@@ -0,0 +1,3 @@
+.\objects\pll.o: PLL.c
+.\objects\pll.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\pll.o: PLL.h
---
+++ ToF/2dx4_ds90-1/Objects/uart.d
@@ -0,0 +1,5 @@
+.\objects\uart.o: uart.c
+.\objects\uart.o: uart.h
+.\objects\uart.o: tm4c1294ncpdt.h
+.\objects\uart.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
